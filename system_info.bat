9110b1ff44186f99807a6fff062231fde8520327
Francisco Fernandez <92254869+FFernandez1@users.noreply.github.com>
2024-02-14 15:45:18 +0100

    Update system_info.bat

--- system_info.bat
+++ system_info.bat
@@ -10,12 +10,10 @@ if exist "%outputFile%" del "%outputFile%"
 :: Output system information
 systeminfo > "%outputFile%"
 
-
 :: Retrieve Mozilla Firefox version 
 for /f "tokens=3" %%k in ('reg query "HKLM\SOFTWARE\mozilla.org\Mozilla" /v CurrentVersion 2^>nul ^| findstr /i "CurrentVersion"') do (
     echo Firefox Version: %%k >> "%outputFile%"
 )
-
 
 :: Retrieve Chrome version 
 for /f "tokens=3" %%i in ('reg query "HKLM\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Google Chrome" /v DisplayVersion 2^>nul ^| findstr /i "DisplayVersion"') do (
@@ -26,7 +24,6 @@ for /f "tokens=3" %%i in ('reg query "HKLM\Software\Wow6432Node\Microsoft\Window
 for /f "tokens=3" %%j in ('reg query "HKLM\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\BraveSoftware Brave-Browser" /v DisplayVersion 2^>nul ^| findstr /i "DisplayVersion"') do (
     echo Brave Version: %%j >> "%outputFile%"
 )
-
 
 echo ...
 echo File generated: "%outputFile%"
